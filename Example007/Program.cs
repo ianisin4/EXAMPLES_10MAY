b70d1048e68d29466276fc82625a7128e39112c6
Irina <Anisinaira@yandex.ru>
2022-05-12 13:21:34 +0400

    Внесли функцию

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -1,16 +1,24 @@
-﻿int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
 
-int max = a;
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 2311;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23555;
+int c3 = 33;
 
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-if (d > max) max = d;
-if (e > max) max = e;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 
-Console.Write("max = ");
 Console.Write(max);
